544c9a7c550907d88a8740efb9d05604adca63ce
Jesse Ericksen <jesseericksen@Jesses-MBP.hsd1.wa.comcast.net>
2018-06-05 15:20:58 -0700

    revised header with ifndef

--- Assignments/Assignment3/shm.h
+++ Assignments/Assignment3/shm.h
@@ -4,9 +4,14 @@
    *Requires server.c and client.c in same directory. 
 */
 
+#ifndef SHM_H
+#define SHM_H
+
 enum StatusEnum{ INVALID, VALID, CONSUMED };
 
 typedef struct{
 	enum StatusEnum status;
 	int data;
 } ShmData;
+
+#endif 
